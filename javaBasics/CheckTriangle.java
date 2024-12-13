b1eec51b34cdc0ec1095fd0c19ef03f1034ae8b1
Sonu Kumar S <sonukumar.dev2000@gmail.com>
2024-12-13 07:18:19 +0530

    Updated triangle and prime number logic; adjusted project settings

--- javaBasics/CheckTriangle.java
+++ javaBasics/CheckTriangle.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class CheckTriangle {
+import java.util.Scanner;
+
+/**
+ * You are given the lengths of 3 sides of a valid triangle. You need to print any one of the following outputs depending on the triangle's nature.
+ * Print 1, if the triangle is equilateral
+ * Print 0, if it's isosceles
+ * Print -1, if it's scalene
+ * Draw a flowchart for this process.
+Note : You don't need to submit the problem. Just attempt in your notebook and ask doubts if you need help.
+ */
+
+public class CheckTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+//        Input the sides of the triangle
+        System.out.println("Enter the length of side a: ");
+        int a = scanner.nextInt();
+        System.out.println("Enter the length of side b: ");
+        int b = scanner.nextInt();
+        System.out.println("Enter the length of side c: ");
+        int c = scanner.nextInt();
+
+//        Conditionals for checking
+        if(a==b && b==c){
+            System.out.println(1);
+        } else if (a==b || b==c || c==a) {
+            System.out.println(0);
+        }else{
+            System.out.println(-1);
+        }
+
+        scanner.close();
+    }
 }
+
+
--- javaBasics/PrimeNumbers.java
+++ javaBasics/PrimeNumbers.java
@@ -1,2 +1,29 @@
-public class AllPrime {
+import java.util.Scanner;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+//        Input the number to get prime numbers
+        System.out.print("Enter a positive number: ");
+        int N = scanner.nextInt();
+
+//        print all prime numbers
+        System.out.println("Prime numbers from 1 to "+ N +":");
+        for(int i =2; i <= N; i++){
+                if(isPrime(i)){
+                    System.out.print(i+" ");
+                }
+        }
+    }
+
+//    Method to check whether a number is prime
+public static boolean isPrime(int num){
+        if (num <= 1)   return false;
+
+        for (int i=2; i<=Math.sqrt(num); i++){
+            if (num % i == 0) return false;
+        }
+        return true;
+   }
 }
